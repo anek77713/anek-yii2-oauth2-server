c937e9896292531c073eb3736860f259773dca05
anek77713 <aleks.shyshoff@mail.ru>
2015-10-23 13:27:05 +0300

    added check by user_id to token action

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -5,6 +5,8 @@ namespace anek77713\yii2\oauth2server\controllers;
 use Yii;
 use yii\helpers\ArrayHelper;
 use anek77713\yii2\oauth2server\filters\ErrorToExceptionFilter;
+use anek77713\yii2\oauth2server\models\OauthClients;
+use dektrium\user\models\User;
 
 class DefaultController extends \yii\rest\Controller
 {
@@ -24,8 +26,20 @@ class DefaultController extends \yii\rest\Controller
     {
         $server = $this->module->getServer();
         $request = $this->module->getRequest();
+
+        $oauthClientModel = new OauthClients();
+        $client = $oauthClientModel::find()->where(['client_id' => 'testclient'])->one();
+
+        $userModel = new User();
+        $user = $userModel::find()->where(['email' => $request->request("username")])->one();
+
+        if ($client->client_id && $user->id) {
+            if ($user->id !== $client->user_id) {
+                throw new \yii\web\HttpException(400, 'Invalid user and client credential combination', 0);
+            }
+        }
+
         $response = $server->handleTokenRequest($request);
-        
         return $response->getParameters();
     }
 }
